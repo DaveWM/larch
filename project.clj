e3fa496b49fce224ee0755f8989b0d8a9b901a4d
David Martin <david.martin@fundingcircle.com>
2019-02-20 12:14:05 +0000

    Updated circleci config to 2.0

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject larch "0.1.0"
+(defproject larch "0.1.1-SNAPSHOT"
   :description "An elm-like clojurescript framework"
   :url "An elm-like clojurescript framework"
   :license {:name "GPL V3"
             :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
   :plugins [[lein-cljsbuild "1.1.5"]
-            [lein-doo "0.1.7"]]
-  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
-                 [org.clojure/clojurescript "1.9.494"]
-                 [org.clojure/core.async "0.3.442"]]
+            [lein-doo "0.1.10"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/clojurescript "1.10.520"]
+                 [org.clojure/core.async "0.4.490"]]
   :cljsbuild {
               :builds [{:id "prod"
                         :source-paths ["src"]
